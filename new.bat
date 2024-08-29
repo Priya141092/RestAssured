400eaf364ff514b5a90e9e64d555fabbcc2acebd
Priya141092 <priyasaxena1492@gmail.com>
2024-08-29 18:49:28 +0530

    Adding rest assured

---
+++ new.bat
@@ -0,0 +1,19 @@
+@echo off
+cd C:\Users\priyasaxena\eclipse-workspace\PetStoreFramework 
+mvn clean install
+pause
+
+
+----------------------------
+
+J9021- 30th October 2023 HCM Qualified 
+
+MEL - 2024 31st dec
+
+MEL submit assessment  - failed 
+
+
+
+
+
+
